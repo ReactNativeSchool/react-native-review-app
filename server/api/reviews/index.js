54daf6fe0adfa313c81d1a881469fcc4f0b40bb3
Spencer Carli <spencercarli@gmail.com>
2019-06-25 09:43:15 -0500

    Ability to create review

--- server/api/reviews/index.js
+++ server/api/reviews/index.js
@@ -1,6 +1,7 @@
 const app = require("../../util/configureApi");
 const connectDB = require("../../util/db");
 const Review = require("../../models/Review");
+const Restaurant = require("../../models/Restaurant");
 
 app.get("*", (req, res) => {
   connectDB()
@@ -10,7 +11,30 @@ app.get("*", (req, res) => {
         throw new Error("No document id specified.");
       }
 
-      return Review.find({ restaurantId });
+      return Review.find({ restaurantId }).sort({ createdAt: -1 });
+    })
+    .then(result => {
+      res.status(200).json({
+        result
+      });
+    })
+    .catch(err => {
+      res.status(err.statusCode || 500).json({
+        error: err.message
+      });
+    });
+});
+
+app.post("*", (req, res) => {
+  connectDB()
+    .then(() => Restaurant.findOne({ _id: req.body.restaurantId }))
+    .then(restaurant => {
+      if (!restaurant) {
+        throw new Error("No restaurant found with that id.");
+      }
+
+      const { restaurantId, content } = req.body;
+      return Review.create({ restaurantId, content });
     })
     .then(result => {
       res.status(200).json({
--- server/models/Review.js
+++ server/models/Review.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" }
+  restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
+  createdAt: { type: Date, default: Date.now }
   // userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
