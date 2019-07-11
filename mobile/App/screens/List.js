c7050d8b02f3a44bf6bad0db56cac2e3d9720f6b
Spencer Carli <spencercarli@gmail.com>
2019-07-11 10:21:36 -0500

    Finish UI

--- mobile/App/screens/List.js
+++ mobile/App/screens/List.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React from "react";
+import { ActivityIndicator } from "react-native";
 
-import {List, ListItem} from '../components/List';
+import { List, ListItem } from "../components/List";
 
 const DATA = [
   {
-      "_id": "5d14d8360ac4c1fab0fe8999",
-      "name": "Restaurant 1",
-      "hours": "8am - 8pm",
-      "address": "123 Fake Street",
-      "__v": 0
+    _id: "5d14d8360ac4c1fab0fe8999",
+    name: "Restaurant 1",
+    hours: "8am - 8pm",
+    address: "123 Fake Street",
+    __v: 0
   },
   {
-      "_id": "5d14d8360ac4c1fab0fe899a",
-      "name": "Restaurant 2",
-      "hours": "1pm - 11pm",
-      "address": "000 Hmm Drive",
-      "__v": 0
+    _id: "5d14d8360ac4c1fab0fe899a",
+    name: "Restaurant 2",
+    hours: "1pm - 11pm",
+    address: "000 Hmm Drive",
+    __v: 0
   },
   {
-      "_id": "5d14d8360ac4c1fab0fe899b",
-      "name": "Restaurant 3",
-      "hours": "7am - 7pm",
-      "address": "111 Real Street",
-      "__v": 0
+    _id: "5d14d8360ac4c1fab0fe899b",
+    name: "Restaurant 3",
+    hours: "7am - 7pm",
+    address: "111 Real Street",
+    __v: 0
   }
-]
+];
 
-export default ({ navigation }) => (
-  <List
-    data={DATA}
-    renderItem={({ item, index }) => (
-      <ListItem
-        title={item.name}
-        isOdd={index % 2}
-        onPress={() => navigation.navigate("RestaurantDetails", { item })}
+export default class ListScreen extends React.Component {
+  state = {
+    restaurants: DATA,
+    restaurantsLoading: false
+  };
+
+  render() {
+    if (this.state.restaurantsLoading) {
+      return <ActivityIndicator />;
+    }
+
+    return (
+      <List
+        data={this.state.restaurants}
+        renderItem={({ item, index }) => (
+          <ListItem
+            title={item.name}
+            isOdd={index % 2}
+            onPress={() =>
+              this.props.navigation.navigate("RestaurantDetails", { item })
+            }
+          />
+        )}
       />
-    )}
-  />
-);
+    );
+  }
+}
--- mobile/App/screens/RestaurantDetails.js
+++ mobile/App/screens/RestaurantDetails.js
@@ -1,6 +1,16 @@
-
 import React from "react";
-import { View, StyleSheet, SafeAreaView, Text, ScrollView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  ActivityIndicator,
+  TextInput
+} from "react-native";
+import { format } from "date-fns";
+
+import { Button } from "../components/Button";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,7 +24,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#E4E4E4",
     marginBottom: 20,
-    padding: 14,
+    padding: 14
   },
   titleText: {
     fontWeight: "600",
@@ -25,21 +35,67 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 16,
-    color: "#4A4A4A",
+    color: "#4A4A4A"
   },
   textBold: {
-    fontWeight: '700'
+    fontWeight: "700"
+  },
+  review: {
+    marginBottom: 10
+  },
+  dateText: {
+    fontSize: 12,
+    color: "#4A4A4A",
+    marginBottom: 4
+  },
+  textInput: {
+    marginBottom: 20,
+    color: "#828282"
   }
 });
 
+const REVIEWS = [
+  {
+    _id: "5d14d8370ac4c1fab0fe899d",
+    content: "This is the second review for Restaurant 1!",
+    restaurantId: "5d14d8360ac4c1fab0fe8999",
+    createdAt: "2019-06-27T14:52:39.268Z",
+    __v: 0
+  },
+  {
+    _id: "5d14d8370ac4c1fab0fe899c",
+    content: "This is the first review for Restaurant 1!",
+    restaurantId: "5d14d8360ac4c1fab0fe8999",
+    createdAt: "2019-06-27T14:52:39.268Z",
+    __v: 0
+  },
+  {
+    _id: "5d14d8370ac4c1fab0fe899e",
+    content: "This is the third review for Restaurant 1!",
+    restaurantId: "5d14d8360ac4c1fab0fe8999",
+    createdAt: "2019-06-27T14:52:39.268Z",
+    __v: 0
+  }
+];
+
 class RestaurantDetails extends React.Component {
+  state = {
+    reviewsLoading: false,
+    reviews: REVIEWS,
+    newReview: ""
+  };
+
+  submitReview = () => {
+    this.setState({ newReview: "" });
+  };
+
   render() {
     const item = this.props.navigation.getParam("item", {});
 
     return (
       <SafeAreaView style={styles.container}>
         <ScrollView>
-          <Text style={styles.titleText}></Text>
+          <Text style={styles.titleText} />
           <View style={styles.section}>
             <Text style={styles.text}>
               <Text style={styles.textBold}>Address:</Text>
@@ -53,11 +109,27 @@ class RestaurantDetails extends React.Component {
 
           <Text style={styles.titleText}>Reviews</Text>
           <View style={styles.section}>
-            <Text style={styles.text}>{item.description}</Text>
+            {this.state.reviewsLoading && <ActivityIndicator />}
+            {this.state.reviews.map(review => (
+              <View key={review._id} style={styles.review}>
+                <Text style={styles.dateText}>
+                  {format(review.createdAt, "MMMM Do, YYYY")}
+                </Text>
+                <Text style={styles.text}>{review.content}</Text>
+              </View>
+            ))}
           </View>
 
           <Text style={styles.titleText}>Write a Review</Text>
           <View style={styles.section}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Write a review..."
+              numberOfLines={5}
+              onChangeText={newReview => this.setState({ newReview })}
+              value={this.state.newReview}
+            />
+            <Button text="Submit" onPress={this.submitReview} />
           </View>
         </ScrollView>
       </SafeAreaView>
