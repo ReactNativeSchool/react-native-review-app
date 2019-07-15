9936ca693002cc1423f63ccc7499e3b5e97ead2b
Spencer Carli <spencercarli@gmail.com>
2019-07-15 10:36:33 -0500

    More things to remove

--- server/models/Review.js
+++ server/models/Review.js
@@ -4,7 +4,6 @@ const ReviewSchema = new mongoose.Schema({
   content: { type: String, required: true },
   restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" },
   createdAt: { type: Date, default: Date.now }
-  // userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
