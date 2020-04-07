5b2f0f18b308a6c0344c0e2012f7e25eeae6a10b
Spencer Carli <spencercarli@gmail.com>
2020-04-07 09:49:59 -0500

    Update deps

--- mobile/App/index.js
+++ mobile/App/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StatusBar, TouchableOpacity, Text } from "react-native";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+// import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createCompatNavigatorFactory,
+  createSwitchNavigator,
+} from "@react-navigation/compat";
 
 import List from "./screens/List";
 import RestaurantDetails from "./screens/RestaurantDetails";
@@ -10,12 +15,12 @@ import CreateAccount from "./screens/CreateAccount";
 
 const defaultStackOptions = {
   headerStyle: {
-    backgroundColor: "#D22322"
+    backgroundColor: "#D22322",
   },
-  headerTintColor: "#fff"
+  headerTintColor: "#fff",
 };
 
-const Information = createStackNavigator(
+const Information = createCompatNavigatorFactory(createStackNavigator)(
   {
     List: {
       screen: List,
@@ -25,55 +30,53 @@ const Information = createStackNavigator(
           <TouchableOpacity onPress={() => navigation.navigate("Auth")}>
             <Text style={{ color: "#fff", marginRight: 10 }}>Sign Out</Text>
           </TouchableOpacity>
-        )
-      })
+        ),
+      }),
     },
     RestaurantDetails: {
       screen: RestaurantDetails,
       navigationOptions: ({ navigation }) => ({
-        headerTitle: navigation.getParam("item", {}).name
-      })
-    }
+        headerTitle: navigation.getParam("item", {}).name,
+      }),
+    },
   },
   {
     defaultNavigationOptions: {
-      ...defaultStackOptions
-    }
+      ...defaultStackOptions,
+    },
   }
 );
 
-const Auth = createStackNavigator(
+const Auth = createCompatNavigatorFactory(createStackNavigator)(
   {
     CreateAccount: {
       screen: CreateAccount,
       navigationOptions: {
-        headerTitle: "Create Account"
-      }
+        headerTitle: "Create Account",
+      },
     },
     SignIn: {
       screen: SignIn,
       navigationOptions: {
-        headerTitle: "Sign In"
-      }
-    }
+        headerTitle: "Sign In",
+      },
+    },
   },
   {
     defaultNavigationOptions: {
-      ...defaultStackOptions
-    }
+      ...defaultStackOptions,
+    },
   }
 );
 
 const App = createSwitchNavigator({
   Auth,
-  Information
+  Information,
 });
 
-const AppWithContainer = createAppContainer(App);
-
 export default () => (
-  <React.Fragment>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" />
-    <AppWithContainer />
-  </React.Fragment>
+    <App />
+  </NavigationContainer>
 );
